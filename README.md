6ea236ffe43e1b1311dac7a27e790aa7daced4eb
SimonHeimberg <simon.heimberg@heimberg-ea.ch>
2017-02-03 08:04:12 +0100

    create package with a composer command

---
+++ README.md
@@ -0,0 +1,18 @@
+This package contains common code for developing the CUBE Tools.
+
+Installation
+============
+
+Step 1: Download the Package
+----------------------------
+
+Open a command console, enter your project directory and execute the
+following command to download the latest stable version of this bundle:
+
+```console
+$ composer require --dev cubetools/cube-common-develop
+```
+
+This command requires you to have Composer installed globally, as explained
+in the [installation chapter](https://getcomposer.org/doc/00-intro.md)
+of the Composer documentation.
---
+++ src/ComposerCommands.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace CubeTools\CubeCommonDevelop;
+
+use Incenteev\ParameterHandler\ScriptHandler;
+use Composer\Script\Event;
+
+class ComposerCommands
+{
+    /**
+     * Composer command to ask the user for unset test parameters.
+     *
+     * The parameter file name is taken from the incenteev-parameters. The
+     * original name (often parameters.yml) is modified to include _test before
+     * the file ending (often resulting in parameters_test.yml).
+     */
+    public static function buildTestParameters(Event $event)
+    {
+        if (count($event->getArguments()) === 1) {
+            $postfix = $event->getArguments()[0];
+        } else {
+            $postfix = '_test';
+        }
+        $package = $event->getComposer()->getPackage();
+        $extras = $package->getExtra();
+        $config = $extras['incenteev-parameters'];
+        if (array_keys($config)[0] === 0) {
+            // it is an array of configs
+            $config = $config[0];
+        }
+        $config['file'] = str_replace('.yml', $postfix.'.yml', $config['file']);
+        $config['keep-outdated'] = true;
+        $extras['incenteev-parameters'] = array($config);
+        $package->setExtra($extras);
+
+        return ScriptHandler::buildParameters($event);
+    }
+}
