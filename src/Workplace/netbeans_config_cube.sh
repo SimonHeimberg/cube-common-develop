c9eca0869bc8303f6581dbf2408bbf9c208b30af
SimonHeimberg <simon.heimberg@heimberg-ea.ch>
2017-03-07 11:19:06 +0100

    [Workplace] update nbproject in background thread

--- src/Workplace/netbeans_config_cube.sh
+++ src/Workplace/netbeans_config_cube.sh
@@ -4,26 +4,33 @@
 
 if [ -d nbproject ] || [ -d ~/.netbeans/ ] || type netbeans 2>&1 >/dev/null
 then # netbeans is installed
-    nbUrl=https://github.com/SimonHeimberg/nbproject_4cube
-    if [ -d nbproject/.git ]
-    then # git repo in place
-        git -C nbproject fetch
-        git -C nbproject merge --ff-only
-    elif [ ! -d nbproject ]
-    then # nothing there
-        git clone $nbUrl nbproject
-    else # settings there
-        atExit() {
-            [ -d .tmp_nbproject ] && mv -b .tmp_nbproject /tmp/tmp_nbproject
-        }
-        trap 'atExit' EXIT
-        git clone $nbUrl .tmp_nbproject &&
-        mv -i .tmp_nbproject/.git nbproject && # place repo into config
-        rm -r .tmp_nbproject
-        git -C nbproject checkout -- $(git -C nbproject diff --name-only --diff-filter=D)
-        git -C nbproject --no-pager diff --exit-code || echo check your netbeans configuration
-    fi
-    cp -n nbproject/project.xml.dist nbproject/project.xml # copy project xml if it does not exist
+    installNetbeansSettings () {
+        nbUrl=https://github.com/SimonHeimberg/nbproject_4cube
+        {
+            if [ -d nbproject/.git ]
+            then # git repo in place
+                git -C nbproject fetch
+                git -C nbproject merge --ff-only
+            elif [ ! -d nbproject ]
+            then # nothing there
+                git clone $nbUrl nbproject
+            else # settings there
+                atExit() {
+                    [ -d .tmp_nbproject ] && mv -b .tmp_nbproject /tmp/tmp_nbproject
+                }
+                trap 'atExit' EXIT
+                git clone $nbUrl .tmp_nbproject &&
+                mv -i .tmp_nbproject/.git nbproject && # place repo into config
+                rm -r .tmp_nbproject
+                git -C nbproject checkout -- $(git -C nbproject diff --name-only --diff-filter=D)
+                git -C nbproject --no-pager diff --exit-code || echo check your netbeans configuration
+            fi
+        } | sed -e '1 i\\nupdating nbproject ...'
+        cp -n nbproject/project.xml.dist nbproject/project.xml # copy project xml if it does not exist
+        echo updating nbproject finished
+    }
+    installNetbeansSettings & # run in background, TODO do not show job id
+    disown -h # is not killed on exit of shell
 fi
 
 installGitHook () {
@@ -39,4 +46,7 @@ installGitHook () {
         [ -f .git/hooks/pre-commit ] || cp -n "$checkStyle" .git/hooks/pre-commit
     fi
 }
+
 installGitHook $1
+
+jobs %% 2>/dev/null && sleep 2 # wait a bit to allow jobs to be finished
