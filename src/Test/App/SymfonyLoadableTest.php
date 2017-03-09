79224cc05da5e602dcdabff9dc428c097280a1f8
SimonHeimberg <simon.heimberg@heimberg-ea.ch>
2017-03-09 17:00:02 +0100

    [Test] add test for checking app.php, app_dev.php and .../console

---
+++ src/Test/App/SymfonyLoadableTest.php
@@ -0,0 +1,60 @@
+<?php
+
+namespace CubeTools\CubeCommonDevelop\Test\App;
+
+use Symfony\Component\Process\Process;
+use Symfony\Component\Process\PhpExecutableFinder;
+
+/**
+ * Basic test to check if symfony binaries are loadable.
+ *
+ * Extend this class in a test of the project, or create a test file containing "new SymfonyLoadableTest();"
+ *
+ * Tests if web/app.php, web/app_dev.php and xxx/console are runnable.
+ * Usable when modifying autoloading or console location, or ...
+ */
+class SymfonyLoadableTest extends \PHPUnit_Framework_TestCase
+{
+    /**
+     *
+     * @var string php executable path
+     */
+    private static $php;
+
+    public static function setUpBeforeClass()
+    {
+        $executableFinder = new PhpExecutableFinder();
+        self::$php = $executableFinder->find();
+    }
+
+    /**
+     * @dataProvider getAppNames
+     */
+    public function testAppRunnable($appName)
+    {
+        $p = new Process(self::$php.' web/'.$appName, null, null, 5);
+        $p->mustRun();
+        $this->assertEquals('', $p->getErrorOutput(), 'no error output');
+        $this->assertNotEmpty($p->getOutput(), 'some output');
+    }
+
+    public static function getAppNames()
+    {
+        return array(array('app.php'), array('app_dev.php'));
+    }
+
+    public function testConsoleRunnable()
+    {
+        $console = 'bin/console';
+        if (!file_exists($console)) {
+            $console = 'app/console';
+        }
+        $p = new Process(self::$php.' '.$console.' -V', null, null, 5);
+        $p->mustRun();
+        $this->assertEquals('', $p->getErrorOutput(), 'no error output');
+        $this->assertContains('ersion', $p->getOutput(), 'some output');
+        if ('bin/' === substr($console, 0, 4)) {
+            $this->assertTrue(is_dir('var'), 'var/ exists if console in bin/');
+        }
+    }
+}
