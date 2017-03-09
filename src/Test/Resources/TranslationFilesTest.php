0b3eaf157dabd11a04f6dbfb6c5427e89aec1899
SimonHeimberg <simon.heimberg@heimberg-ea.ch>
2017-03-09 15:48:52 +0100

    WIP marked TODO -- [Test] Test for translation files

--- src/Test/Resources/TranslationFilesTest.php
+++ src/Test/Resources/TranslationFilesTest.php
@@ -19,13 +19,13 @@ class TranslationFilesTest extends KernelTestCase
         static::bootKernel();
         $tr = static::$kernel->getContainer()->get('translator');
         $cat = $tr->getCatalogue($langName);
-        $texts = array('label.addresslist.user.phone'); # TODO read from settings? xlf file? $cat->getResources() $path = xxx->getResource ...  
+        $texts = array('label.addresslist.user.phone'); # TODO read from settings? xlf file? $cat->getResources() $path = xxx->getResource ...
         foreach ($texts as $text) {
             $r = $cat->defines($text);
             $msg = 'translation problem';
             if (!$r) {
                 $msg = sprintf('translation of "%s" missing', $text);
-                if ($langName == 'it') { # TODO read from ...  
+                if ($langName == 'it') { # TODO read from ...
                     $this->markTestSkipped($msg);
                 }
             }
@@ -36,7 +36,7 @@ class TranslationFilesTest extends KernelTestCase
 
     public static function listLanguages()
     {
-        # TODO automatic check if all are listed (from {app,src,...}/Resources/translations/{messages,...}.*.x*)  
+        # TODO automatic check if all are listed (from {app,src,...}/Resources/translations/{messages,...}.*.x*)
         $languages = array('en', 'de', 'fr', 'it');
         foreach ($languages as $lang) {
             yield $lang => array($lang);
