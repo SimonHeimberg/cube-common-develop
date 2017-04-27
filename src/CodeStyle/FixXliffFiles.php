9dc5f65e9719a5ee0e184e78b5d74046c808b62c
SimonHeimberg <simon.heimberg@heimberg-ea.ch>
2017-04-27 11:14:17 +0200

    [CodeStyle] add command lint:xliff to fix translation files
    Checks ids currently.

---
+++ src/CodeStyle/FixXliffFiles.php
@@ -0,0 +1,99 @@
+<?php
+
+namespace CubeTools\CubeCommonDevelop\CodeStyle;
+
+use Symfony\Component\Console\Command\Command;
+use Symfony\Component\Console\Input\InputArgument;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Output\OutputInterface;
+use Symfony\Component\DomCrawler\Crawler;
+
+class FixXliffFiles extends Command
+{
+    public function fixXliffFile($fileName, $doFix = false)
+    {
+        if (!file_exists($fileName)) {
+            return array('file not found');
+        }
+        $content = \file_get_contents($fileName);
+        $crawler = new Crawler();
+        $crawler->addXMLContent($content);
+        $fixed = array();
+        $crawler->filter('body trans-unit')->each(function ($unit) use (&$fixed) {
+            $this->checkUnit($unit, $fixed);
+        });
+        if ($fixed) {
+            $xmlDoc = $crawler->getNode(0)->ownerDocument;
+            $xmlDoc->encoding = 'utf-8';
+            $bytes = \file_put_contents($fileName.'#', $xmlDoc->saveXML());
+            if ($doFix && $bytes) {
+                rename($fileName, $fileName.'~');
+                rename($fileName.'#', $fileName);
+            } elseif ($doFix) {
+                $fixed[] = 'FAILED to write file';
+            }
+        }
+
+        return $fixed;
+    }
+
+    protected function configure()
+    {
+        $this
+            ->setName('lint:xliff:cubestyle')
+            ->addArgument('files', InputArgument::IS_ARRAY | InputArgument::REQUIRED, 'files to check')
+        ;
+    }
+
+    protected function execute(InputInterface $input, OutputInterface $output)
+    {
+        $files = $input->getArgument('files');
+        $nErrors = 0;
+        $eFiles = 0;
+        $cFiles = 0;
+        foreach ($files as $file) {
+            ++$cFiles;
+            $output->write($file);
+            $errors = $this->fixXliffFile($file);
+            if ($errors) {
+                $n = count($errors);
+                $output->writeln(sprintf(' <error>%d ERRORS</>', $n));
+                $nErrors += $n;
+                foreach ($errors as $error) {
+                    $msg = sprintf(
+                        ' * <comment>%s</>',
+                        $error
+                    );
+                    $output->write($msg);
+                }
+                ++$eFiles;
+            } else {
+                $output->writeln(' <info>[OK]</>');
+            }
+        }
+        if ($nErrors) {
+            $msg = '<comment>%s</>: <error>%d Errors</> in <error>%d</> files (checked %d of %d)';
+            $output->writeln(\sprintf($msg, $this->getName(), $nErrors, $eFiles, $cFiles, \count($files)));
+        } else {
+            $output->writeln(\sprintf('%s: <info>[OK] checked %d files</>', $this->getName(), \count($files)));
+        }
+
+        return $nErrors;
+    }
+
+    private function checkUnit($unit, array &$fixed)
+    {
+        $id = $unit->attr('id');
+        $sourceTxt = $unit->filter('source')->text();
+        if (false === strpos($sourceTxt, $id) && false === strpos(strtolower($sourceTxt), $id)) {
+            $spacePos = strpos($sourceTxt, ' %');
+            if (false !== $spacePos) {
+                $nId = substr($sourceTxt, 0, $spacePos);
+            } else {
+                $nId = $sourceTxt;
+            }
+            $unit->getNode(0)->setAttribute('id', $nId);
+            $fixed[] = 'id of "'.substr(strtr($sourceTxt, array("\n" => "\\n")), 0, 128).'"';
+        }
+    }
+}
---
+++ src/SymfonyCommands.php
@@ -0,0 +1,38 @@
+#!/usr/bin/env php
+<?php
+
+namespace CubeTools\CubeCommonDevelop;
+
+use Symfony\Component\Console\Application;
+use CubeTools\CubeCommonDevelop\CodeStyle;
+
+function addCcdCommands($application)
+{
+    $application->add(new CodeStyle\FixXliffFiles());
+}
+
+function initCommands()
+{
+    $bootstrap = null;
+    $appDir = __DIR__.'/../../../../../';
+    $bootstrapDirs = array('./var/', './app/', $appDir.'var/', $appDir.'app/');
+    $bootstrapDirs[] = $bootstrapDirs[0];
+    foreach ($bootstrapDirs as $bootstrapDir) {
+        $bootstrap = $bootstrapDir.'bootstrap.php.cache';
+        if (file_exists($bootstrap)) {
+            break;
+        }
+    }
+    require_once $bootstrap;
+
+    $application = new Application();
+    addCcdCommands($application);
+
+    return $application->run();
+}
+
+$_mainFile = !\debug_backtrace(DEBUG_BACKTRACE_IGNORE_ARGS, 1);
+if ($_mainFile) {
+    $r = initCommands();
+    echo $r;
+}
